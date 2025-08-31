02c6c4b3251392902b64fb93e90f40a2d2460046
Noman Amjad <nq6145841@gmail.com>
2025-08-31 11:27:44 +0500

    "fix:Make_createsuperuser_command_idempotent"

--- home/management/commands/create_admin.py
+++ home/management/commands/create_admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Command(BaseCommand):
-    help = 'Creates an admin user non-interactively from environment variables.'
+    help = 'Creates or updates an admin user from environment variables.'
 
     def handle(self, *args, **options):
         username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
@@ -13,11 +13,16 @@ class Command(BaseCommand):
         password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
         if not all([username, email, password]):
-            self.stdout.write(self.style.ERROR('Missing superuser environment variables. Skipping superuser creation.'))
+            self.stdout.write(self.style.ERROR('Missing superuser environment variables. Skipping superuser creation/update.'))
             return
 
         if User.objects.filter(username=username).exists():
-            self.stdout.write(self.style.SUCCESS(f'Admin user "{username}" already exists.'))
+            self.stdout.write(f'Admin user "{username}" already exists. Ensuring password and email are up to date.')
+            user = User.objects.get(username=username)
+            user.set_password(password)
+            user.email = email
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f'Admin user "{username}" updated successfully.'))
         else:
             self.stdout.write(f'Creating admin user "{username}"...')
             User.objects.create_superuser(username=username, email=email, password=password)
